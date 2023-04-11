28138a16c7e7587465f776125f7189004e96668e
MsPwesh <ogwepreciouso@gmail.com>
2023-04-11 20:14:31 +0100

    scientific cal

---
+++ script.js
@@ -0,0 +1,15 @@
+let calculator_button = [
+{
+    name: "delete",
+    symbol: <i class="fa-thin fa-delete-left"></i>,
+    formular: false,
+    type: "key",
+},
+
+{
+    name : "multiplication",
+    symbol : "x",
+    formular : "*", 
+    type : "operator" 
+}
+]
